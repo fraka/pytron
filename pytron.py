b2cf41a3cefe072313b418bd063c53f2c55d6e95
fraka <matfraca@gmail.com>
2010-02-03 07:54:34 +0100

    moving to object oriented code

--- pytron.py
+++ pytron.py
@@ -1,31 +1,209 @@
+# Source code released under gpl v3 licence, see COPYING file
+
 from pyglet import window, clock, image
 from pyglet.gl import *
 from pyglet.window import key
-from random import choice
+from random import choice, randint
+
+#Bonus
+class Bonus:
+  def __init__(self, id, type, color, coord):
+    global grid
+    self.id = id
+    self.type = type
+    self.x, self.y = coord
+    self.color = color
+
+#snake
+class Snake:
+
+  def __init__(self, id, type, keys, color, coord):
+    global grid
+    self.id = id
+    self.type = type
+    if type == 'drone':
+      self.min_tail = 0
+      self.max_tail = 0
+    else:
+      self.min_tail = 9
+      self.max_tail = 299
+    self.tail = self.min_tail
+    self.x, self.y = coord
+    self.dir = choice((0,1,2,3))
+    self.points = 0
+    self.reset = False
+    self.up, self.right, self.down, self.left = keys
+    self.color = color
+
+  def select_direction(self, grid):
+    global cpu_ai, cpu_avoid
+    if self.type == 'drone':
+      self.dir = choice(cpu_ai[snake.dir])
+    elif self.type == 'cpu':
+      avoid_length = choice((4,4,4,8,8))
+      if self.dir == 0:
+        avoid_y = snake.y + avoid_length
+        if avoid_y > grid_height - 1 or grid.get_point(self.x,avoid_y) != (0, 0):
+          self.dir = choice(cpu_avoid[0])
+        else:
+          self.dir = choice(cpu_ai[0])
+      elif self.dir == 1:
+        avoid_x = self.x + avoid_length
+        if avoid_x > grid_width - 1 or grid.get_point(avoid_x,self.y) != (0, 0):
+          self.dir = choice(cpu_avoid[1])
+        else:
+          self.dir = choice(cpu_ai[1])
+      elif self.dir == 2:
+        avoid_y = self.y - avoid_length
+        if avoid_y < 0 or grid.get_point(self.x,avoid_y) != (0, 0):
+          self.dir = choice(cpu_avoid[2])
+        else:
+          self.dir = choice(cpu_ai[2])
+      elif self.dir == 3:
+        avoid_x = self.x - avoid_length
+        if avoid_x < 0 or grid.get_point(avoid_x,self.y) != (0, 0):
+          self.dir = choice(cpu_avoid[3])
+        else:
+          self.dir = choice(cpu_ai[3])
+
+  def move(self, grid):
+    if self.dir == 0:
+      if self.y < grid.height - 1: self.y+=1
+      else:                        self.y=0
+    elif self.dir == 1:
+      if self.x < grid.width - 1:  self.x+=1
+      else:                        self.x=0
+    elif self.dir == 2:
+      if self.y > 0:               self.y-=1
+      else:                        self.y=grid.height - 1
+    elif self.dir == 3:
+      if self.x > 0:               self.x-=1
+      else:                        self.x=grid.width - 1
+
+  def check_collision(self, grid):
+    global snakesArray
+    state, age = grid.get_point(self.x,self.y)
+    if state == 0:
+      grid.set_point(self.x,self.y,(self.id,0))
+    elif state >= 1 and state <= 20:
+      if self.type == 'drone':
+        grid.set_point(self.x,self.y,(self.id,0))
+      else:
+        self.reset = True
+        self.tail = self.min_tail
+        if self.id == self.id:
+          if self.points >= 100:
+            self.points -= 100
+          else:
+            self.points = 0  
+        else:
+            snake = snakesArray[state]
+            snake.points += 100
+    elif state >= 21 and state <= 40:
+      grid.set_point(self.x,self.y,(self.id,0))
+      if state == 21:
+        self.points += 10
+        self.tail += 10
+        if self.tail > self.max_tail:
+          self.tail = self.max_tail
+      elif state == 22:
+        self.points += 20
+        self.tail -= 10
+        if self.tail < self.min_tail:
+          self.tail = self.min_tail
+    elif state == 255:
+      self.reset = True
+      self.tail = self.min_tail
+      if self.points >= 100:
+        self.points -= 100
+      else:
+        self.points = 0
+  
+
+#Grid
+class Grid:
+
+  def __init__(self, width, height):
+    self.width = width
+    self.height = height
+    self.data = [[(0,0)]*self.width for i in range(self.height)]
+    
+  def get_point(self, x, y):
+    return self.data[y][x]
+    
+  def set_point(self, x, y, value):
+    self.data[y][x] = value
+    
+  def reset_point(self, x, y):
+    self.data[y][x] = (0,0)
+    
+  def random_point(self):
+    bx = randint(0, self.width - 1)
+    by = randint(0, self.height - 1)
+    for y in range(self.height):
+      by = (by + y) % self.height
+      for x in range(self.width):
+        bx = (bx + x) % self.width
+        if self.data[by][bx] == (0, 0):
+          return (bx, by)
+    return Null, Null
+    
+  def show_bonus(self):
+    global bonus
+    b = choice(bonus)  
+    if b != 0:
+      bx, by = self.random_point()
+      self.set_point(bx,by,(b,0))
+      
+      
 
 def draw_grid():
-  global square_verts, grid, colors, snakes, grid_width, grid_height, square_size, arena_border
-  glBegin(GL_QUADS)  
-  for y in range(grid_height):
-    for x in range(grid_width):
-      state, age = grid[y][x]
-      if state != 0:
-        snake = snakes[state - 1]
-        if snake['reset'] or age > snake['tail']:
-          grid[y][x] = (0, 0)
+  global snakesArray, grid, squares_verts, colors, bonus_timeout
+  verts_coord = []
+  verts_color = []
+  for y in range(grid.height):
+    for x in range(grid.width):
+      state, age = grid.get_point(x, y)
+      color_index = 0
+      fade = 1
+      if state >= 1 and state <= 20:
+        snake = snakesArray[state - 1]
+        if snake.reset or age > snake.tail:
+          grid.reset_point(x,y)
         else:
-          grid[y][x] = (state, age + 1)
-          #snake['points'] += 1 
-          #fade = 1 - (0.04 * age)
-          #if fade < 0.2: fade= 0.2
-          fade=1
-          r, g, b = colors[state]
-          glColor3f(r * fade, g * fade, b * fade)
-          for vx,vy in square_verts:
-            vx += arena_border + (x * square_size)
-            vy += arena_border + (y * square_size)
-            glVertex2f(vx, vy)
-  glEnd()
+          grid.set_point(x,y,(state, age + 1))
+          color_index = snake.color
+          fade -= 0.005 * age
+          if fade < 0.4: fade= 0.4
+      elif state >= 21 and state <= 40:
+        if age > bonus_timeout:
+          grid.reset_point(x,y)
+        else:
+          grid.set_point(x,y,(state, age + 1))
+          if state == 21:
+            color_index = 11
+          else:
+            color_index = 12
+      elif state == 255:
+        color_index = 10
+      if color_index > 0:
+        r, g, b = colors[color_index]
+        r *= fade
+        g *= fade
+        b *= fade
+        verts_color.extend([r,g,b,r,g,b,r,g,b,r,g,b])
+        verts_coord.extend(squares_verts[y][x])
+  verts_coord_size = len(verts_coord)
+  verts_color_size = len(verts_color) 
+  verts_coord_gl = (GLfloat * verts_coord_size)(*verts_coord)
+  verts_color_gl = (GLfloat * verts_color_size)(*verts_color)
+  glEnableClientState(GL_VERTEX_ARRAY);
+  glEnableClientState(GL_COLOR_ARRAY);
+  glColorPointer(3, GL_FLOAT, 0, verts_color_gl)
+  glVertexPointer(2, GL_FLOAT, 0, verts_coord_gl);
+  glDrawArrays(GL_QUADS, 0, verts_coord_size // 2)
+  glDisableClientState(GL_VERTEX_ARRAY);
+  glDisableClientState(GL_COLOR_ARRAY);
 
 def draw_arena():
   global arena_verts
@@ -41,6 +219,11 @@ def draw_header():
   glColor3f(1, 1, 1)
   header_img.blit(arena_border, arena_border + arena_border + arena_height)
 
+def draw_background():
+  global background_img, arena_border, arena_height
+  glColor3f(1, 1, 1)
+  background_img.blit(arena_border, arena_border)
+
 screen_width = 740
 screen_height = 550
 
@@ -53,12 +236,26 @@ square_size = 8
 grid_width = arena_width / square_size 
 grid_height = arena_height / square_size
 
+#8 = max 5400 squares
+
 win = window.Window(visible=False,width=screen_width,height=screen_height)
 win.set_visible()
 header_img = image.load('header.png').texture
+background_img = image.load('background.png').texture
 fps_limit = 15
 clock.set_fps_limit(fps_limit)
-keyboard = key.KeyStateHandler()
+#keyboard = key.KeyStateHandler()
+
+@win.event
+def on_key_press(symbol, modifiers):
+  global snakesArray
+  for snake in snakesArray:
+    if snake.type == 'human':
+      if symbol == snake.up and snake.dir != 2:      snake.dir = 0
+      elif symbol == snake.right and snake.dir != 3: snake.dir = 1
+      elif symbol == snake.down and snake.dir != 0:  snake.dir = 2
+      elif symbol == snake.left and snake.dir != 1:  snake.dir = 3
+#win.on_key_press = on_key_press
 
 arena_verts =  [
   (arena_border-1,arena_border-2),
@@ -74,152 +271,94 @@ square_verts = [
   (0,square_size-1)
 ]
 
-grid = [[(0,0)]*grid_width for i in range(grid_height)]
+squares_verts = []
+for y in range(grid_height):
+  squares_verts.append([])
+  for x in range(grid_width):
+    squares_verts[y].append([])
+    for vx,vy in square_verts:
+      squares_verts[y][x].append(vx + arena_border + (x * square_size))
+      squares_verts[y][x].append(vy + arena_border + (y * square_size))
 
+#(id, age)
+# id = 0          : empty
+# id >= 1 e <=20  : snake (human, cpu, drone)
+# id >=21 e <=40  : bonus
+# id = 255        : wall
+grid = Grid(grid_width, grid_height)
+
+#set wall
+for y in [10, 11, 48, 49]:
+  for x in range(20, 69):
+    grid.set_point(x,y,(255,0))
+
+bonus = (0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,21,22)
 cpu_ai = [
   (0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,3),
   (1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,0),
   (2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,3,3,1),
   (3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,0,0,2)
 ]
-
 cpu_avoid = [
   (1,1,1,3),
   (2,2,2,0),
   (3,3,3,1),
   (0,0,0,2)
 ]
-
 colors = [
-  (  0,   0,   0),
-  (  1,   0,   0),
-  (  0,   1,   0),
-  (  0,   0,   1),
-  (  1,   1,   0),
-  (  1,   0,   1),
-  (  0,   1,   1),
-  (  1, 0.5, 0.5),
-  (0.5, 0.5,   1),
-  (1,     1,   1),
-  (1,     1,   1),
-  (1,     1,   1),
-  (1,     1,   1)        
+  (0,0,0),
+  (0,0,1),
+  (1,1,0),
+  (1,0,1),
+  (0,1,1),
+  (0.5,0.5,0),
+  (0.5,0,0.5),
+  (0,0.5,0.5),
+  (1,1,1),
+  (1,1,1),
+  (1,1,1),
+  (0,1,0),
+  (1,0,0)
 ]
 
-#off, human, cpu, drone
-snakes = [
-  {'id':  1,'type':   'cpu','tail':199,'x': 1,'y': 1,'dir': 0,'points': 0,'reset': False,'up': key.UP,'right': key.RIGHT,'down': key.DOWN,'left': key.LEFT},
-  {'id':  2,'type':   'cpu','tail':199,'x': 2,'y': 2,'dir': 0,'points': 0,'reset': False,'up': key.W ,'right': key.D    ,'down': key.S   ,'left': key.A},
-  {'id':  3,'type':   'cpu','tail':199,'x': 3,'y': 3,'dir': 0,'points': 0,'reset': False,'up': key.T ,'right': key.H    ,'down': key.G   ,'left': key.F},
-  {'id':  4,'type':   'cpu','tail':199,'x': 4,'y': 4,'dir': 0,'points': 0,'reset': False,'up': key.I ,'right': key.L    ,'down': key.K   ,'left': key.J},
-  {'id':  5,'type':   'cpu','tail':199,'x': 5,'y': 5,'dir': 0,'points': 0,'reset': False,'up': 0,'right': 0,'down': 0,'left': 0},
-  {'id':  6,'type':   'cpu','tail':199,'x': 6,'y': 6,'dir': 0,'points': 0,'reset': False,'up': 0,'right': 0,'down': 0,'left': 0},
-  {'id':  7,'type':   'off','tail': 29,'x': 7,'y': 1,'dir': 0,'points': 0,'reset': False,'up': 0,'right': 0,'down': 0,'left': 0},
-  {'id':  8,'type':   'off','tail': 29,'x': 8,'y': 2,'dir': 0,'points': 0,'reset': False,'up': 0,'right': 0,'down': 0,'left': 0},
-  {'id':  9,'type':   'off','tail': 29,'x': 9,'y': 3,'dir': 0,'points': 0,'reset': False,'up': 0,'right': 0,'down': 0,'left': 0},
-  {'id': 10,'type':   'off','tail': 29,'x':10,'y': 4,'dir': 0,'points': 0,'reset': False,'up': 0,'right': 0,'down': 0,'left': 0},
-  {'id': 11,'type':   'off','tail': 29,'x':11,'y': 5,'dir': 0,'points': 0,'reset': False,'up': 0,'right': 0,'down': 0,'left': 0},
-  {'id': 12,'type':   'off','tail': 29,'x':12,'y': 6,'dir': 0,'points': 0,'reset': False,'up': 0,'right': 0,'down': 0,'left': 0}
-]
+snakesArray = []
+snakesArray.append(Snake(1,'cpu',(key.UP,key.RIGHT,key.DOWN,key.LEFT),1,grid.random_point()))
+snakesArray.append(Snake(2,'cpu',(key.W,key.D,key.S,key.A),2,grid.random_point()))
+snakesArray.append(Snake(3,'cpu',(key.R,key.G,key.F,key.D),3,grid.random_point()))
+snakesArray.append(Snake(4,'cpu',(key.U,key.K,key.J,key.H),4,grid.random_point()))
+snakesArray.append(Snake(5,'cpu',(0,0,0,0),5,grid.random_point()))
+snakesArray.append(Snake(6,'cpu',(0,0,0,0),6,grid.random_point()))
 
-for snake in snakes:
-  if snake['type'] != 'off': grid[snake['y']][snake['x']] = (snake['id'], 0)
+#bonusArray = [] 
+bonus_timeout = 74
+
+for snake in snakesArray:
+  grid.set_point(snake.x,snake.y,(snake.id,0))
 
 while not win.has_exit:
   win.dispatch_events()
-  win.push_handlers(keyboard)
-  
   dt = clock.tick()
   
-  #win.set_caption('Pytron v0.2 - 1: %d, 2: %d, 3: %d, 4: %d - (fps: %s)' % (snakes[0]['points'], snakes[1]['points'], snakes[2]['points'], snakes[3]['points'], round(clock.get_fps())))
-  win.set_caption('Pytron v0.2 (fps: %s)' % (round(clock.get_fps())))
+  win.set_caption('Pytron v0.3 (fps: %s)' % (round(clock.get_fps())))
 
   glClear(GL_COLOR_BUFFER_BIT)
   glLoadIdentity()
   
   draw_header()
+  draw_background()
   draw_arena()
   
-  for snake in snakes:
-    snake['reset'] = False
-    if snake['type'] == 'off':
-      continue
-    elif snake['type'] == 'drone':
-      snake['dir'] = choice(cpu_ai[snake['dir']])
-    elif snake['type'] == 'cpu':
-      avoid_length = choice((2,4,6,8,10))
-      if snake['dir'] == 0:
-        avoid_y = snake['y'] + avoid_length
-        if avoid_y > grid_height - 1 or grid[avoid_y][snake['x']] != (0, 0): snake['dir'] = choice(cpu_avoid[0])
-        else:                                                                snake['dir'] = choice(cpu_ai[0])
-      elif snake['dir'] == 1:
-        avoid_x = snake['x'] + avoid_length
-        if avoid_x > grid_width - 1 or grid[snake['y']][avoid_x] != (0, 0):  snake['dir'] = choice(cpu_avoid[1])
-        else:                                                                snake['dir'] = choice(cpu_ai[1])
-      elif snake['dir'] == 2:
-        avoid_y = snake['y'] - avoid_length
-        if avoid_y < 0 or grid[avoid_y][snake['x']] != (0, 0):               snake['dir'] = choice(cpu_avoid[2])
-        else:                                                                snake['dir'] = choice(cpu_ai[2])
-      elif snake['dir'] == 3:
-        avoid_x = snake['x'] - avoid_length
-        if avoid_x < 0 or grid[snake['y']][avoid_x] != (0, 0):               snake['dir'] = choice(cpu_avoid[3])
-        else:                                                                snake['dir'] = choice(cpu_ai[3])
-    else:
-      if keyboard[snake['up']] and snake['dir'] != 2:      snake['dir'] = 0
-      elif keyboard[snake['right']] and snake['dir'] != 3: snake['dir'] = 1
-      elif keyboard[snake['down']] and snake['dir'] != 0:  snake['dir'] = 2
-      elif keyboard[snake['left']] and snake['dir'] != 1:  snake['dir'] = 3
-      
-    if snake['dir'] == 0:
-      if snake['y'] < grid_height - 1:
-        snake['y']+=1
-      else:
-        snake['y']=0
-        #snake['dir'] = 2
-        #snake['reset'] = True
-    elif snake['dir'] == 1:
-      if snake['x'] < grid_width - 1:
-        snake['x']+=1
-      else:
-        snake['x']=0
-        #snake['dir'] = 3
-        #snake['reset'] = True
-    elif snake['dir'] == 2:
-      if snake['y'] > 0:
-        snake['y']-=1
-      else:
-        snake['y']=grid_height - 1
-        #snake['dir'] = 0
-        #snake['reset'] = True
-    elif snake['dir'] == 3:
-      if snake['x'] > 0:
-        snake['x']-=1
-      else:
-        snake['x']=grid_width - 1
-        #snake['dir'] = 1
-        #snake['reset'] = True
-    
-    if not snake['reset']:
-      state, age = grid[snake['y']][snake['x']]
-      #if state == 0 or (snake['type'] == 'drone' and state == snake['id']):
-      if state == 0 or snake['type'] == 'drone':
-        grid[snake['y']][snake['x']] = (snake['id'],0)
-        snake['points'] += 1
-      else:
-        snake['reset'] = True
-#        if state == snake['id']:
-#          if snake['points'] >= 100:
-#            snake['points'] -= 100
-#          else:
-#            snake['points'] = 0  
-#        else:
-#          snakes[state-1]['points'] += 100  
-
+  grid.show_bonus()
+  
+  for snake in snakesArray:
+    snake.reset = False
+    snake.select_direction(grid)
+    snake.move(grid)
+    snake.check_collision(grid)
+  
   draw_grid()
-
   win.flip()
 
-print "Punteggio\n"
-for snake in snakes:
-  print snake['id'], ": ", snake['points']
-   
+print "Punteggio"
+for snake in snakesArray:
+  print snake.id, ": ", snake.points
